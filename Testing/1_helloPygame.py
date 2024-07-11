b2a0231cc33103fafff66a70e6b5b023d97b24fb
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-11 16:09:30 +0100

    Update and rename helloPygame.py to 1_helloPygame.py

--- Testing/1_helloPygame.py
+++ Testing/1_helloPygame.py
@@ -1,9 +1,11 @@
+""" Setting up screen/window in PyGame & OpenGL """
 import pygame
 from pygame.locals import *
 
 pygame.init()
 screen_width, screen_height = 1000, 800
 
+# setup screen in pygame, use OpenGL double-buffering
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
 pygame.display.set_caption('OpenGL in Python')
 
