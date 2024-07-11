be25fb274a31387d8d08a8b741d56dd1de4a0c24
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-07-11 16:24:32 +0100

    Update 2_helloOpenGL.py

--- Testing/2_helloOpenGL.py
+++ Testing/2_helloOpenGL.py
@@ -1,3 +1,4 @@
+""" Draw point on screen with OpenGL """
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
@@ -9,11 +10,29 @@ screen_width, screen_height = 1000, 800
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
 pygame.display.set_caption('OpenGL in Python')
 
+
+def init_ortho():
+    """ Setup 2D Graphic Generation (orthographic) Mode """
+    glMatrixMode(GL_PROJECTION)  # for camera/world projection
+    glLoadIdentity()  # clear GL_PROJECTION & provide new palette
+    gluOrtho2D(0, 640, 0, 480)  # set window coords (x1, x2, y1, y2)
+
+
 done = False
+init_ortho()
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()  # clear GL_MODELVIEW
+    glPointSize(5)  # set size of points on screen
+    glBegin(GL_POINTS)  # every glBegin needs a matching glEnd
+    glVertex2i(100, 50)  # put a point at x,y coord
+    glEnd()
+
     pygame.display.flip()
     pygame.time.wait(100)
 pygame.quit()
